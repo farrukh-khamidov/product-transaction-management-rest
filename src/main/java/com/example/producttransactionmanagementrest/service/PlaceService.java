e3bb275aa1abc15a5553322f119c8eddd5505191
Farrukh <farrukh.khamidov@upay.uz>
2022-11-26 13:46:41 +0500

    evaluate done

--- src/main/java/com/example/producttransactionmanagementrest/service/PlaceService.java
+++ src/main/java/com/example/producttransactionmanagementrest/service/PlaceService.java
@@ -1,11 +1,10 @@
 package com.example.producttransactionmanagementrest.service;
 
 import com.example.producttransactionmanagementrest.entity.Place;
-import com.example.producttransactionmanagementrest.generics.CrudService;
-import com.example.producttransactionmanagementrest.generics.JpaService;
 
 import java.util.List;
 
-public interface PlaceService extends JpaService<Place, Long> {
+public interface PlaceService {
 
+    List<Place> findAll();
 }
--- src/main/java/com/example/producttransactionmanagementrest/service/impl/PlaceServiceImpl.java
+++ src/main/java/com/example/producttransactionmanagementrest/service/impl/PlaceServiceImpl.java
@@ -1,26 +1,22 @@
 package com.example.producttransactionmanagementrest.service.impl;
 
 import com.example.producttransactionmanagementrest.entity.Place;
-import com.example.producttransactionmanagementrest.generics.CrudServiceImpl;
-import com.example.producttransactionmanagementrest.generics.JpaServiceImpl;
 import com.example.producttransactionmanagementrest.repository.PlaceRepository;
 import com.example.producttransactionmanagementrest.service.PlaceService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
 @Service
 @RequiredArgsConstructor
-public class PlaceServiceImpl extends JpaServiceImpl<Place, Long> implements PlaceService {
+public class PlaceServiceImpl implements PlaceService {
 
     private final PlaceRepository placeRepository;
 
-
     @Override
-    public JpaRepository<Place, Long> getRepository() {
-        return placeRepository;
+    public List<Place> findAll() {
+        return placeRepository.findAll();
     }
 }
